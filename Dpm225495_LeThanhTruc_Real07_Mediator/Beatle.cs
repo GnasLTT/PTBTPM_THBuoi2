6e5bc956784347551903f95c0d9bc9a2983ea1c4
Truc268 <144977485+GnasLTT@users.noreply.github.com>
2025-09-30 10:41:24 +0700

    Add files via upload

---
+++ Dpm225495_LeThanhTruc_Real07_Mediator/Beatle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dpm225495_LeThanhTruc_Real07_Mediator
+{
+    internal class Beatle : Participant
+    {
+        // Constructor
+        public Beatle(string name)
+            : base(name)
+        {
+        }
+        public override void Receive(string from, string message)
+        {
+            Console.Write("To a Beatle: ");
+            base.Receive(from, message);
+        }
+    }
+}
---
+++ Dpm225495_LeThanhTruc_Real07_Mediator/NonBeatle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dpm225495_LeThanhTruc_Real07_Mediator
+{
+    internal class NonBeatle : Participant
+    {
+        // Constructor
+        public NonBeatle(string name)
+            : base(name)
+        {
+        }
+        public override void Receive(string from, string message)
+        {
+            Console.Write("To a non-Beatle: ");
+            base.Receive(from, message);
+        }
+    }
+    
+}
