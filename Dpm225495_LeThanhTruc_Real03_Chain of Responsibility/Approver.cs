a1bc4f8bd013ed39419b1ffd93e24e29e32d7f3c
Truc268 <144977485+GnasLTT@users.noreply.github.com>
2025-09-30 10:38:11 +0700

    Add files via upload

---
+++ Dpm225495_LeThanhTruc_Real03_Chain of Responsibility/Approver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dpm225495_LeThanhTruc_Real03_Chain_of_Responsibility
+{
+    public abstract class Approver
+    {
+        protected Approver successor;
+        public void SetSuccessor(Approver successor)
+        {
+            this.successor = successor;
+        }
+        public abstract void ProcessRequest(Purchase purchase);
+    }
+}
---
+++ Dpm225495_LeThanhTruc_Real03_Chain of Responsibility/Purchase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dpm225495_LeThanhTruc_Real03_Chain_of_Responsibility
+{
+    internal class Purchase
+    {
+        int number;
+        double amount;
+        string purpose;
+        // Constructor
+        public Purchase(int number, double amount, string purpose)
+        {
+            this.number = number;
+            this.amount = amount;
+            this.purpose = purpose;
+        }
+        // Gets or sets purchase number
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+        // Gets or sets purchase amount
+        public double Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+        // Gets or sets purchase purpose
+        public string Purpose
+        {
+            get { return purpose; }
+            set { purpose = value; }
+        }
+    }
+}
