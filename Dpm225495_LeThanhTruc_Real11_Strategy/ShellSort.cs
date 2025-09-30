e703cb5e1e2f7fd3f0ed1e2c33c0dc123f6074b1
Truc268 <144977485+GnasLTT@users.noreply.github.com>
2025-09-30 10:42:57 +0700

    Add files via upload

---
+++ Dpm225495_LeThanhTruc_Real11_Strategy/ShellSort.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dpm225495_LeThanhTruc_Real11_Strategy
+{
+    internal class ShellSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            //list.ShellSort();  not-implemented
+            Console.WriteLine("ShellSorted list ");
+        }
+    }
+}
---
+++ Dpm225495_LeThanhTruc_Real12_Template_Method/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dpm225495_LeThanhTruc_Real12_Template_Method
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            DataAccessor categories = new Categories();
+            categories.Run(5);
+            DataAccessor products = new Products();
+            products.Run(3);
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
