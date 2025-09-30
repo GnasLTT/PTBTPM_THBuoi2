9a27de4fe612bcdbd8f0e3b234e93fde5602c1e2
Truc268 <144977485+GnasLTT@users.noreply.github.com>
2025-09-30 10:31:52 +0700

    Add files via upload

---
+++ Dpm225495_LeThanhTruc_Pattern01_Flyweight/ConcreteFlyweight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dpm225495_LeThanhTruc_Pattern01_Flyweight
+{
+    internal class ConcreteFlyweight : Flyweight
+    {
+        public override void Operation(int extrinsicstate)
+        {
+            Console.WriteLine("ConcreteFlyweight: " + extrinsicstate);
+        }
+    }
+}
